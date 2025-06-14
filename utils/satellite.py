310ae164e6d68214cf2b8e223c40980558077b74
Brandon Hall <brandonhall.email@gmail.com>
2025-06-14 17:48:36 -0400

    scale fix attempt

--- utils/satellite.py
+++ utils/satellite.py
@@ -51,7 +51,7 @@ def download_tif(lat_min, lon_min, lat_max, lon_max, tif_path):
             .mosaic() \
             .select(['R', 'G', 'B']) \
             .clip(region)
-        scale = 4
+        scale = 3.6
     else:
         print("🌍 Using Sentinel-2 SR Harmonized imagery")
         image = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED") \
@@ -62,7 +62,7 @@ def download_tif(lat_min, lon_min, lat_max, lon_max, tif_path):
             .median() \
             .select(['B4', 'B3', 'B2']) \
             .clip(region)
-        scale = 4
+        scale = 3.6
         
     download_url = image.getDownloadURL({
         'region': region,
